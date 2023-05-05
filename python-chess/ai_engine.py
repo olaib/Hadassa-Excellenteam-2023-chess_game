1ff2fe74b12d6f6cbfb8dc2dfa1bfe00371021c3
Ola Ibrahim <92628525+olaib@users.noreply.github.com>
2023-05-05 23:30:26 +0300

    some fixes

---
+++ python-chess/ai_engine.py
@@ -0,0 +1,161 @@
+#
+# The Chess AI class
+# Will utilize minimax and alpha beta pruning
+#
+# Author: Boo Sung Kim
+# Note: Code inspired from the pseudocode by Sebastian Lague
+# from enums import Player
+# TODO: switch undo moves to stack data structure
+import chess_engine
+from enums import Player
+
+# added by me
+PIECE_VALUES = {
+    "k": {"white": 1000, "black": -1000},
+    "q": {"white": 100, "black": -100},
+    "r": {"white": 50, "black": -50},
+    "b": {"white": 30, "black": -30},
+    "n": {"white": 30, "black": -30},
+    "p": {"white": 10, "black": -10},
+}
+
+
+class chess_ai:
+    '''
+    call minimax with alpha beta pruning
+    evaluate board
+    get the value of each piece
+    '''
+
+    def minimax_white(self, game_state, depth, alpha, beta, maximizing_player, player_color):
+        csc = game_state.checkmate_stalemate_checker()
+        if maximizing_player:
+            if csc == 0:
+                return 5000000
+            elif csc == 1:
+                return -5000000
+            elif csc == 2:
+                return 100
+        elif not maximizing_player:
+            if csc == 1:
+                return 5000000
+            elif csc == 0:
+                return -5000000
+            elif csc == 2:
+                return 100
+
+        if depth <= 0 or csc != 3:
+            return self.evaluate_board(game_state, Player.PLAYER_1)
+
+        if maximizing_player:
+            max_evaluation = -10000000
+            all_possible_moves = game_state.get_all_legal_moves("black")
+            for move_pair in all_possible_moves:
+                game_state.move_piece(move_pair[0], move_pair[1], True)
+                evaluation = self.minimax_white(game_state, depth - 1, alpha, beta, False, "white")
+                game_state.undo_move()
+
+                if max_evaluation < evaluation:
+                    max_evaluation = evaluation
+                    best_possible_move = move_pair
+                alpha = max(alpha, evaluation)
+                if beta <= alpha:
+                    break
+            if depth == 3:
+                return best_possible_move
+            else:
+                return max_evaluation
+        else:
+            min_evaluation = 10000000
+            all_possible_moves = game_state.get_all_legal_moves("white")
+            for move_pair in all_possible_moves:
+                game_state.move_piece(move_pair[0], move_pair[1], True)
+                evaluation = self.minimax_white(game_state, depth - 1, alpha, beta, True, "black")
+                game_state.undo_move()
+
+                if min_evaluation > evaluation:
+                    min_evaluation = evaluation
+                    best_possible_move = move_pair
+                beta = min(beta, evaluation)
+                if beta <= alpha:
+                    break
+            if depth == 3:
+                return best_possible_move
+            else:
+                return min_evaluation
+
+    def minimax_black(self, game_state, depth, alpha, beta, maximizing_player, player_color):
+        csc = game_state.checkmate_stalemate_checker()
+        if maximizing_player:
+            if csc == 1:
+                return 5000000
+            elif csc == 0:
+                return -5000000
+            elif csc == 2:
+                return 100
+        elif not maximizing_player:
+            if csc == 0:
+                return 5000000
+            elif csc == 1:
+                return -5000000
+            elif csc == 2:
+                return 100
+
+        if depth <= 0 or csc != 3:
+            return self.evaluate_board(game_state, Player.PLAYER_2)
+
+        if maximizing_player:
+            max_evaluation = -10000000
+            all_possible_moves = game_state.get_all_legal_moves("white")
+            for move_pair in all_possible_moves:
+                game_state.move_piece(move_pair[0], move_pair[1], True)
+                evaluation = self.minimax_black(game_state, depth - 1, alpha, beta, False, "black")
+                game_state.undo_move()
+
+                if max_evaluation < evaluation:
+                    max_evaluation = evaluation
+                    best_possible_move = move_pair
+                alpha = max(alpha, evaluation)
+                if beta <= alpha:
+                    break
+            if depth == 3:
+                return best_possible_move
+            else:
+                return max_evaluation
+        else:
+            min_evaluation = 10000000
+            all_possible_moves = game_state.get_all_legal_moves("black")
+            for move_pair in all_possible_moves:
+                game_state.move_piece(move_pair[0], move_pair[1], True)
+                evaluation = self.minimax_black(game_state, depth - 1, alpha, beta, True, "white")
+                game_state.undo_move()
+
+                if min_evaluation > evaluation:
+                    min_evaluation = evaluation
+                    best_possible_move = move_pair
+                beta = min(beta, evaluation)
+                if beta <= alpha:
+                    break
+            if depth == 3:
+                return best_possible_move
+            else:
+                return min_evaluation
+
+    def evaluate_board(self, game_state, player):
+        evaluation_score = 0
+        for row in range(0, 8):
+            for col in range(0, 8):
+                if game_state.is_valid_piece(row, col):
+                    evaluated_piece = game_state.get_piece(row, col)
+                    evaluation_score += self.get_piece_value(evaluated_piece, player)
+        return evaluation_score
+
+    # make it more efficient by using a dictionary
+    def get_piece_value(self, piece, player):
+        player_color = "white" if player == Player.PLAYER_1 else "black"
+        piece_color = "white" if piece.is_player(Player.PLAYER_1) else "black"
+        piece_name = piece.get_name()
+        # fix bug - signs exchanged => for black was negative, for white was positive
+        # now it's the opposite
+        return -PIECE_VALUES[piece_name][piece_color] if piece_color == player_color else PIECE_VALUES[piece_name][
+            piece_color]
